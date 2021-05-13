610ea6dff62963c49be841d8ae7768feba5dc1da
Andrew Whelan <whelandrew@gmail.com>
2021-05-13 15:20:53 -0700

    vertical jump

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 //parent for character controls (all character-related classes go through here)
@@ -9,111 +7,131 @@ using UnityEngine;
 //stats
 public class Controller : MonoBehaviour
 {
-    private float noInputTimer = 0;
+    private Vector2 velocity = Vector2.zero;
+    private Vector2 position = Vector2.zero;
 
-    private Rigidbody2D rigidBody;
-    private CapsuleCollider2D cCollider;    
+    public float maxVelocity = 5f;
+    public float dampening = 0f;
+
+    //for tracking keys used in movement
+    public KeyCode[] MappedKeys = new KeyCode[] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Space, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow };
+
+    CapsuleCollider2D cCollider;
+    Rigidbody2D rBody;
+
+    private bool canMove;
+
+    private bool isGrounded;    
+    private bool isFalling;
+
+    public bool IsGrounded
+    {
+        get { return isGrounded; }
+        set { isGrounded = value; }
+    }
 
     private Vector2 direction = Vector2.zero;
     private Vector2 moveDir = Vector2.zero;
-
-    private bool stopMovement;
-    private bool isGrounded;
-    private bool isJumping;
-    Vector2 groundTarget = Vector2.zero;
-    Vector2 jumpTarget = Vector2.zero;
-
     private float moveSpeed = 7f;
+
+    private float inputTimer = 0;
+    public float inputTimeMax = 0.03f;
+
     private float fallSpeed = 10f;
 
-    private float jumpVelocity = 8f;
-    private float jumpMagnitude = 8f;
-    
+
+    //jump values
+    private bool isJumping = false;
+    private bool wantsToJump = false;
+    public float timeToApex = 1f;
+    public float jumpHeight = 5f;
+    public float minJumpHeight = 1f;
+    public float gravity = 9.807f;
+    private float elapsed = 0f;
+    private float initJumpVelocity = 0f;
+    private float lastGrounded = 0;
+    public float damp = .5f;
 
     private void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
         cCollider = GetComponent<CapsuleCollider2D>();
+        rBody = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {        
-        if (!StopMovement())
-        {            
-            if (Input.GetKey(KeyCode.Space))
-            {
-                if (isGrounded)
-                    if (!isJumping)
-                        Jumping();
+    private void Update()
+    {
+        elapsed = elapsed + Time.deltaTime;
+        wantsToJump = false;
 
-                if (isJumping)
-                    transform.Translate(jumpTarget * Time.deltaTime);
-            }
-            else
+        //is player controllable
+        if (CanMove())
+        {
+            //is character on the ground
+            if (IsGrounded)
             {
-                //falling
-                if (!isGrounded)
+                Jump();                            
+                //get jump input
+                if (isJumping)
                 {
-                    transform.Translate((Vector2.down * fallSpeed) * Time.deltaTime);
-                    jumpTarget = Vector2.zero;
-                    isJumping = false;
+                    JumpCalc();                    
                 }
                 else
                 {
-                    Moving();
+                    //get move input
+                    Walking();
                 }
+            }
+            else
+            {                
+                JumpTermination();
+                canMove = false;
             }
         }
     }
 
-    private bool StopMovement()
+    private void FixedUpdate()
     {
-        noInputTimer += Time.deltaTime;
-
-        if (!Input.anyKey)
+        if (!IsGrounded && !isJumping)
         {
-            noInputTimer += 1 * Time.deltaTime;
+            //character is falling
+            Fall();
         }
         else
         {
-            stopMovement = false;
-            noInputTimer = 0;
-        }
-
-        if (noInputTimer >= 1)
-        {            
-            if (!isGrounded)
+            if (CanMove())
             {
-                return false;
+                if (isJumping)
+                {
+                    //if pressedJumpKey then
+                    rBody.velocity = new Vector2(transform.position.x, initJumpVelocity);
+                }
+                else
+                {
+                    //rBody.velocity = moveDir * moveSpeed;
+                    if (isFalling)
+                    {
+                        EdgeJump();
+                    }
+                }
             }
             else
             {
-                stopMovement = true;
-                return true;
+                rBody.velocity = Vector2.zero;
             }
         }
-
-        return false;
     }
 
-    private void Jumping()
-    {        
-        isJumping = true;
-        isGrounded = false;
-        groundTarget = transform.position;
-
-        if (jumpTarget == Vector2.zero)
-        {
-            //TODO jump in x directions
-            float xMag = jumpMagnitude * direction.x;
-            jumpTarget = new Vector2(transform.position.x*xMag, transform.position.y + jumpVelocity);
-        }
-
-        //TODO cap jump height
+    private void Reset()
+    {
+        elapsed = 0;
+        wantsToJump = false;
+        canMove = false;
+        isJumping = false;       
     }
 
-    private void Moving()
-    {        
+    private bool Walking()
+    {
+        Debug.Log("Moving");
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             //up
@@ -128,44 +146,162 @@ public class Controller : MonoBehaviour
         {
             //left
             direction = Vector2.left;
-
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             //right
             direction = Vector2.right;
         }
+
         moveDir = direction;
-        //play movement animations   
+        //play movement animations
+
+        return direction != Vector2.zero;
     }
 
-    private void FixedUpdate()
+
+    private void EdgeJump()
     {
-        if (stopMovement)
+        elapsed = 0;
+        lastGrounded = 0;
+
+        if (isGrounded)
         {
-            rigidBody.velocity = Vector2.zero;
+            lastGrounded = elapsed;
+        } 
+
+        if (wantsToJump && (elapsed - lastGrounded < 0.1))
+        {
+            Jump();
         }
-        else
+    }
+    private void Fall()
+    {
+        Debug.Log("Fall");
+        transform.Translate((Vector2.down * fallSpeed) * Time.deltaTime);
+        isFalling = true;
+        Reset();
+    }
+
+    private void JumpCalc()
+    {
+        Debug.Log("JumpCalc");
+        //clamping
+        float v = Mathf.Sqrt((rBody.velocity.x * rBody.velocity.x) + (rBody.velocity.y * rBody.velocity.y));
+        if(v > maxVelocity)
         {
-            if (isGrounded)
+            float vs = maxVelocity / v;
+            Vector2 clamp = new Vector2(rBody.velocity.x * vs, rBody.velocity.y * vs);
+            rBody.velocity = clamp;
+        }
+
+        //Dampening
+        Vector2 dampVelocity = new Vector2((rBody.velocity.x / (1 + damp * Time.deltaTime)), (rBody.velocity.y / (1 + damp * Time.deltaTime)));
+        rBody.velocity = dampVelocity;
+
+        //-- what is the gravity that would allow jumping to a given height?
+        //g = (2*jumpHeight)/(timeToApex^2)   
+        gravity = (2 * jumpHeight) / (timeToApex * timeToApex);
+
+        //-- what is the initial jump velocity? 
+        //initJumpVelocity = math.sqrt(2*g* jumpHeight)
+        initJumpVelocity = Mathf.Sqrt(2 * gravity * jumpHeight);
+
+        //-- how long does it take to reach the maximum height of a jump?
+        //-- note: if "initJumpVelocity" is not a multiple of "g" the maximum height is reached between frames
+        //timeToApex = initJumpVelocity / g
+        timeToApex = initJumpVelocity / gravity;        
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Jumping");
+            isJumping = true;           
+
+            //if lastJump and elapsed - lastJump< 0.2
+                //wantsToJump = true
+            float lastJump = (elapsed + Time.deltaTime);
+            if ((lastJump * elapsed) - lastJump < .2f)
             {
-                rigidBody.velocity = moveDir * moveSpeed;
+                wantsToJump = true;
+            }            
+        }
+    }
+
+    private void JumpTermination()
+    {
+        //timeToApex breached
+        if(elapsed >= timeToApex)
+        {
+            isJumping = false;
+        }
+
+        //--what is the velocity required to terminate a jump ?
+        //--note : only works when "g" is negative
+        //termVelocity = math.sqrt(initJumpVelocity ^ 2 + 2 * g * (jumpHeight - minJumpHeight))
+        float termVelocity = Mathf.Sqrt((initJumpVelocity * initJumpVelocity) + 2 * gravity * (jumpHeight - minJumpHeight));        
+
+
+        //-- how much time is available until a jump can no longer be terminated ?
+        //--note : "minJumpHeight" must be greater than 0
+        //termTime = timeToApex - (2 * (jumpHeight - minJumpHeight) / (initJumpVelocity + termVelocity))
+
+        //if releasedJumpKey then
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            //-- is the player ascending fast enough to allow jump termination ?
+            //if player.yv > termVelocity then
+            //player.yv = termVelocity
+            if(rBody.velocity.y > termVelocity)
+            {
+                Vector2 newVelocity = new Vector2(rBody.velocity.x, termVelocity);
+                rBody.velocity = newVelocity;
             }
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //checks if there has been user input, or pauses movement
+    private bool CanMove()
     {
-        //floor collision
-        isGrounded = (collision.gameObject.layer == 9);
-        if (isGrounded)
+        inputTimer += 1 * Time.deltaTime;
+
+        if (!Input.anyKey)
         {
-           isGrounded = true;
+            Reset();
+            //if no input after i seconds
+            if (inputTimer > inputTimeMax)
+            {
+                //if character is not on the ground
+                canMove = IsGrounded;
+
+                //if no input has been detected
+                Reset();
+            }
         }
+        else
+        {
+            //detecting key input by mapped movementkeys
+            for (int i = 0; i < MappedKeys.Length; i++)
+            {
+                if (Input.GetKey(MappedKeys[i]))
+                {
+                    //reset timer if there is player input
+                    inputTimer = 0;
+                    canMove = true;
+                }
+            }
+        }
+
+        return canMove;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        isGrounded =! (collision.gameObject.layer == 9);
+
     }
 }
---
+++ Assets/Scripts/Player/FootCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootCollider : MonoBehaviour
+{
+    public Controller cController;
+    private CircleCollider2D cCollider;
+
+    private void Start()
+    {        
+        cCollider = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        //floor collision
+        if (collision.gameObject.layer == 9)
+        {
+            Debug.Log(collision.gameObject.layer);
+            cController.IsGrounded = true;
+            //cController.jumpMaxReached = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        cController.IsGrounded = !(collision.gameObject.layer == 9);
+    }
+}
